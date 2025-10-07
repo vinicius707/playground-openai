5e6e9f7f6c9254f4f05e6c032d175708df942d47
Vinicius <viniciussilva707@gmail.com>
2025-10-06 21:03:41 -0300

    feat: input de usuário

--- categorizador.py
+++ categorizador.py
@@ -4,28 +4,39 @@ import os
 
 load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
+prompt_sistema="""
+    Você é um categorizador de produtos.
+    Você deve assumir as categorias presentes na lista abaixo.
+    
+    # Lista de Categorias Válidas
+    - Moda Sustentável
+    - Produtos para o Lar
+    - Beleza Natural
+    - Eletrônicos Verdes
+    
+    # Formato de Saída
+    Produto: Nome do Produto
+    Categoria: apresente a categoria do produto
+    
+    # Exemplo de Saída
+    Produto: Escova elétrica com recarga solar
+    Categoria: Eletrônicos Verdes
+"""
+
+prompt_usuario = input("Apresente o nome de um produto: ")
 
 resposta = cliente.chat.completions.create(
+    model="gpt-5-nano",
     messages=[
         {
             "role":"system",
-            "content": """
-            Classifique o produto abaixo em uma das categorias:
-            Higiene, Pessoal, Moda ou Casa e de uma descrição da categoria.
-            """
+            "content": prompt_sistema
         },
         {
             "role":"user",
-            "content":"""
-            Escova de dentes de bambu
-            """
+            "content":prompt_usuario
         }
     ],
-    model="gpt-5-nano",
-    temperature = 0,
-    max_tokens=200,
-    n = 3
 )
 
-for contador in range(0,3):
-    print(resposta.choices[contador].message.content)
+print(resposta.choices[0].message.content)
